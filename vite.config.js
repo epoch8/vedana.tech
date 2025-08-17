d0d8715e31005299b358621796644327a2d361dd
Andrey Tatarinov <a@tatarinov.co>
2025-08-18 01:30:50 +0400

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // For custom domain vedana.tech
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist'
   }
 })

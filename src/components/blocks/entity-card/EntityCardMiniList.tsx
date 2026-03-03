50a7b130a7a1c7bfb2c58106f3a3f3da01dd8e81
Olga Tatarinova <olga@epoch8.co>
2026-03-03 06:50:27 +0400

    WIP Product Demo

--- src/components/blocks/entity-card/EntityCardMiniList.tsx
+++ src/components/blocks/entity-card/EntityCardMiniList.tsx
@@ -1,11 +1,24 @@
+import React from "react";
+import styles from "./EntityCard.module.css";
+
 type Props = {
   children: React.ReactNode;
   className?: string;
 };
 
-export default function EntityCardMiniList({ children, className }: Props) {
+export default function EntityCardMiniList({
+  children,
+  className
+}: Props) {
   return (
-    <div className={`entity-card-mini-rail ${className ?? ""}`}>
+    <div
+      className={[
+        styles["entity-card-mini-rail"],
+        className
+      ]
+        .filter(Boolean)
+        .join(" ")}
+    >
       {children}
     </div>
   );
